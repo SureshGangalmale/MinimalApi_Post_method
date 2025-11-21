c557cb32c1f361e12d0c0e97639d1b3cb28bed37
sureshgangalmale876 <sureshgangalmale876@gmail.com>
2025-11-21 15:01:38 +0530

    create a saperate file for endpoints

---
+++ MinimalApiDemo/EndPoints/StudentEndpoints.cs
@@ -0,0 +1,17 @@
+﻿using MinimalApiDemo.Models;
+using MinimalApiDemo.Repositories;
+
+namespace MinimalApiDemo.EndPoints
+{
+    public static class StudentEndpoints
+    {
+        public static void MapStudentEndpoints(this IEndpointRouteBuilder app)
+        {
+            app.MapPost("/student", (Student student, StudentRepository repo) =>
+            {
+                repo.Insert(student);
+                return Results.Ok("Student Added successfully");
+            });
+        }
+    }
+}
